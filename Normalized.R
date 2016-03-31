c2bf40955aa35dcdc9473dc11d9f7a8028f79cd0
Darshan Patel <patel.dars@husky.neu.edu>
2016-03-30 20:50:06 -0400

    Comments are added.

--- Normalized.R
+++ Normalized.R
@@ -1,8 +1,13 @@
 library(dplyr)
 
+# Load Training Data
 news_train <- read.csv("/Users/Darshan/Documents/CS 7280 Stats/Project/Data/Cleaned_Train.csv", header = TRUE)
 
+# All Column names
 column_names <- names(news_train)
+
+# Column names which needs to be ignored due to categorical and target feature
+
 ignored_column_names <- c("url", "timedelta", "data_channel_is_lifestyle",
                           "data_channel_is_entertainment", "data_channel_is_bus",
                           "data_channel_is_world", "data_channel_is_socmed",
@@ -10,20 +15,18 @@ ignored_column_names <- c("url", "timedelta", "data_channel_is_lifestyle",
                           "weekday_is_wednesday", "weekday_is_thursday", "weekday_is_friday",
                           "weekday_is_saturday", "weekday_is_sunday", "is_weekend", "shares")
 
-norm_parameters <- scale()
-
-news_train_norm <- news_train %>% mutate_each_(funs(scale),vars=setdiff(column_names,ignored_column_names))
-
 needed_columns <- setdiff(column_names,ignored_column_names)
 
+# Normalized Train Data
 news_train_norm <- news_train %>% mutate_each_(funs(scale),vars=needed_columns)
 
+# Saving standard deviation of the columns which are normalized
 sd_values <- Map(sd, news_train[needed_columns])
-
 write.csv("/Users/Darshan/Documents/CS 7280 Stats/Project/Data/Train_SD_Values.csv", row.names = FALSE, x = sd_values)
 
+# Saving eman of the columns which are normalized
 mean_values <- Map(mean, news_train[needed_columns])
-
 write.csv("/Users/Darshan/Documents/CS 7280 Stats/Project/Data/Train_Mean_Values.csv", row.names = FALSE, x = mean_values)
 
+# Saving Normalized Train Data
 write.csv("/Users/Darshan/Documents/CS 7280 Stats/Project/Data/Normalized_Train.csv", row.names = FALSE, x = news_train_norm)
