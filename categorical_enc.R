73517a2ff4521705ed20c4d579c05223c7ad9a4c
Darshan Patel <patel.dars@husky.neu.edu>
2016-03-31 23:37:46 -0400

    One hot encoding to single categorical variable update

--- categorical_enc.R
+++ categorical_enc.R
@@ -2,14 +2,7 @@ suppressMessages(library(dplyr))
 library(ggplot2)
 library(reshape2)
 
-setwd("/home/gbakie/neu/stat-sp16/project")
-news_train <- read.csv("data/OnlineNewsPopularity.csv", header = TRUE)
-
-train_url <- news_train$url
-train_timedelta <- news_train$timedelta
-
-news_train$timedelta <- NULL
-news_train$url <- NULL
+news_train <- read.csv("/Users/Darshan/Documents/CS 7280 Stats/Project/Data/Cleaned_Cor_Train.csv", header = TRUE)
 
 dow_cols = c("weekday_is_monday", "weekday_is_tuesday", "weekday_is_wednesday",
               "weekday_is_thursday", "weekday_is_friday", "weekday_is_saturday",
@@ -24,3 +17,22 @@ for (dow in dow_cols) {
 }
 
 news_train$cat_dow <- as.factor(news_train$cat_dow)
+
+data_channel_cols = c("data_channel_is_lifestyle", "data_channel_is_entertainment",
+                       "data_channel_is_bus", "data_channel_is_socmed", "data_channel_is_tech",
+                       "data_channel_is_world")
+
+news_train$data_channel <- 0
+
+for (channel in data_channel_cols) {
+  channel_idx <- which(news_train[,channel] == 1)
+  news_train[channel_idx,"data_channel"] <- which(data_channel_cols==channel)
+}
+
+news_train$data_channel <- as.factor(news_train$data_channel)
+
+write.csv("/Users/Darshan/Documents/CS 7280 Stats/Project/Data/Cleaned_Cor_Train.csv", row.names = FALSE, x = news_train)
+
+#filter(news_train, data_channel_is_bus==0, data_channel_is_lifestyle==0, 
+#       data_channel_is_entertainment==0, data_channel_is_socmed==0, 
+#       data_channel_is_tech==0, data_channel_is_world==0)
