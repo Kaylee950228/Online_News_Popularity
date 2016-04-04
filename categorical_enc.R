1e76dccf3cfea276a7c3d03bb427ed454b3d6796
gbakie <gabriel.bakiewicz@gmail.com>
2016-04-04 15:43:07 -0400

    categorical encoding for data_channel

--- categorical_enc.R
+++ categorical_enc.R
@@ -15,7 +15,14 @@ dow_cols = c("weekday_is_monday", "weekday_is_tuesday", "weekday_is_wednesday",
               "weekday_is_thursday", "weekday_is_friday", "weekday_is_saturday",
               "weekday_is_sunday")
 
+data_channel_cols = c("data_channel_is_lifestyle", "data_channel_is_entertainment", 
+                      "data_channel_is_bus", "data_channel_is_socmed", "data_channel_is_tech",
+                      "data_channel_is_world")
+
+
 news_train$cat_dow <- 0
+news_train$data_channel <- 0
+
 
 for (dow in dow_cols) {
     dow_idx = which(news_train[,dow] == 1)
@@ -23,4 +30,13 @@ for (dow in dow_cols) {
     news_train[dow_idx,"cat_dow"] <- which(dow_cols==dow)
 }
 
+for (dc in data_channel_cols) {
+  dc_idx = which(news_train[,dc] == 1)
+  #print(dow_idx)
+  news_train[dc_idx,"data_channel"] <- which(data_channel_cols==dc)
+}
+
+
 news_train$cat_dow <- as.factor(news_train$cat_dow)
+news_train$data_channel <- as.factor(news_train$data_channel)
+
