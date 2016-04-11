28799e954127b668563a1034f6e0d150344dd780
Darshan Patel <patel.dars@husky.neu.edu>
2016-04-11 02:01:58 -0400

    Stepwise selection method completed

--- CrossValidation.R
+++ CrossValidation.R
@@ -87,28 +87,33 @@ for(i in 1:length(models)){
 model_variables
 mses
 R2s
+mean(mses)
+mean(R2s)
 
-summary(lm(shares ~ data_channel + i_kw_max_avg_avg + cat_dow + num_hrefs + 
-             kw_max_max + self_reference_avg_sharess + LDA_02 + 
-             global_subjectivity + LDA_00 + n_unique_tokens + num_self_hrefs + 
-             abs_title_sentiment_polarity + n_tokens_title + abs_title_subjectivity
-           + num_imgs + min_positive_polarity + average_token_length + 
-             title_sentiment_polarity + num_keywords + LDA_01, data=news))
-
-summary(lm(shares ~ data_channel + i_kw_max_avg_avg + cat_dow+num_hrefs + 
-             kw_max_max + self_reference_avg_sharess + LDA_02 + 
-             global_subjectivity + LDA_00 + n_unique_tokens + num_self_hrefs + 
-             abs_title_sentiment_polarity + n_tokens_title + abs_title_subjectivity
-           + num_imgs + min_positive_polarity + average_token_length + 
-             title_sentiment_polarity + num_keywords + LDA_01 + title_subjectivity + 
-             min_negative_polarity  + avg_negative_polarity, data=news))
-
-
+summary(lm(shares ~ data_channel +
+             cat_dow +
+             i_kw_max_avg_avg +
+             self_reference_avg_sharess +
+             i_kw_avg_max_max +
+             num_hrefs +
+             global_subjectivity +
+             LDA_00 +
+             LDA_01 +
+             LDA_02 +
+             num_self_hrefs +
+             i_n_unique_tokens_content +
+             i_title_subjectivity_sentiment_polarity +
+             abs_title_subjectivity +
+             n_tokens_title +
+             min_positive_polarity +
+             num_imgs +
+             average_token_length +
+             title_sentiment_polarity, data=news))
 
 model.summaries <- list()
 min.bic.index <- c()
 max.r2.index <- c()
-
+max.r2 <- c()
 select.var.index <- c()
 
 for (i in 1:K) {
@@ -116,67 +121,48 @@ for (i in 1:K) {
   news_train <- news[folds[[i]],]
   news_val <- news[-folds[[i]],]
   
-  model <- regsubsets(shares ~ data_channel + i_kw_max_avg_avg + cat_dow+num_hrefs + 
-                        kw_max_max + self_reference_avg_sharess + LDA_02 + 
-                        global_subjectivity + LDA_00 + n_unique_tokens + num_self_hrefs + 
-                        abs_title_sentiment_polarity + n_tokens_title + abs_title_subjectivity + 
-                        num_imgs + min_positive_polarity + average_token_length + 
-                        title_sentiment_polarity + num_keywords + LDA_01 + n_tokens_content + 
-                        title_subjectivity + avg_negative_polarity + min_negative_polarity + 
-                        i_kw_max_avg_min + global_sentiment_polarity + max_positive_polarity + 
-                        max_negative_polarity + rate_positive_words + global_rate_negative_words + 
-                        kw_min_max + kw_min_avg + rate_positive_words, 
-                      nbest = 1, nvmax = 43,data=news_train, force.in = c(1:30), 
+  model <- regsubsets(shares ~ data_channel +
+                        cat_dow +
+                        i_kw_max_avg_avg +
+                        self_reference_avg_sharess +
+                        i_kw_avg_max_max +
+                        num_hrefs +
+                        global_subjectivity +
+                        LDA_00 +
+                        LDA_01 +
+                        LDA_02 +
+                        num_self_hrefs +
+                        i_n_unique_tokens_content +
+                        i_title_subjectivity_sentiment_polarity +
+                        abs_title_subjectivity +
+                        n_tokens_title +
+                        min_positive_polarity +
+                        num_imgs +
+                        average_token_length +
+                        title_sentiment_polarity + global_rate_negative_words +
+                        i_rate_pos_glob_sent_polarity +
+                        i_min_avg_negative_pol +
+                        num_keywords +
+                        i_rate_pos_glob_sent_polarity +
+                        global_rate_negative_words +
+                        global_rate_positive_words +
+                        i_kw_min_avg_max +
+                        num_videos +
+                        max_negative_polarity, nbest = 1, nvmax = 39, 
+                      data=news_train, force.in = c(1:29), 
                       method = "exhaustive", really.big = TRUE)
   
   summary.model <- summary(model)
+  
   min.bic.index <- append(which.min(summary.model$bic), min.bic.index)
   max.r2.index <- append(which.max(summary.model$adjr2) ,max.r2.index)
+  max.r2<- append(max(summary.model$adjr2),max.r2)
+  
   model.summaries[[i]] <- summary.model
   
-  select.var.index <- append(which.max(which(summary.model$which[2,])), select.var.index)
+  select.var.index <- append(which.max(which(summary.model$which[1,])), select.var.index)
 }
 
 # Found four additional variable 
 
-# title_subjectivity min_negative_polarity n_tokens_content avg_negative_polarity
-
-# Exhaustive search on the variable which were not selected by all the fold in forward, backward
-# and both directional step wise variable selection method
-# columns <- c("data_channel", "i_kw_max_avg_avg", "cat_dow", "num_hrefs", "kw_max_max", 
-#              "self_reference_avg_sharess", "LDA_02", "global_subjectivity", "LDA_00", 
-#              "n_unique_tokens", "num_self_hrefs", "abs_title_sentiment_polarity", 
-#              "n_tokens_title", "abs_title_subjectivity", "num_imgs", "min_positive_polarity", 
-#              "average_token_length", "title_sentiment_polarity", "num_keywords","LDA_01")
-# 
-# seach_columns <- c("n_tokens_content","title_subjectivity","avg_negative_polarity",
-#                    "min_negative_polarity","i_kw_max_avg_min","global_sentiment_polarity",
-#                    "max_positive_polarity","max_negative_polarity","rate_positive_words",
-#                    "global_rate_negative_words","kw_min_max","kw_min_avg","rate_positive_words")
-# 
-# ignore_columns <- setdiff(setdiff(setdiff(names(news), columns), seach_columns), "shares")
-# 
-# s.force.in <- c(17, 1, 16, 5, 14, 6, 15, 7, 10, 2, 4, 8,9,12,11,13,3,18,19,20,34,35,36,37,38,39,40,41,42)
-# s.force.out <- c(43, 44, 45, 46, 47, 48, 49)
-
-model <- regsubsets(shares ~ data_channel + i_kw_max_avg_avg + cat_dow+num_hrefs + 
-                      kw_max_max + self_reference_avg_sharess + LDA_02 + 
-                      global_subjectivity + LDA_00 + n_unique_tokens + num_self_hrefs + 
-                      abs_title_sentiment_polarity + n_tokens_title + abs_title_subjectivity
-                     + num_imgs + min_positive_polarity + average_token_length + 
-                      title_sentiment_polarity + num_keywords + LDA_01 + n_tokens_content + 
-                      title_subjectivity + avg_negative_polarity + min_negative_polarity + 
-                      i_kw_max_avg_min + global_sentiment_polarity + max_positive_polarity + 
-                      max_negative_polarity + rate_positive_words + global_rate_negative_words + 
-                      kw_min_max + kw_min_avg + rate_positive_words, 
-                    nbest = 1, nvmax = 43,data=news, force.in = c(1:30), 
-                    method = "exhaustive", really.big = TRUE)
-
-# for(column in columns){
-#   s.force.in <- append(which(colnames(news)==column),  s.force.in)
-# }
-# 
-# for(i.column in ignore_columns){
-#   s.force.out <- append(which(colnames(news)==i.column), s.force.out)
-# }
-# regsubsets(shares~.,nbest = 1, nvmax = 4, force.in=s.force.in, force.out = s.force.out, data=news)
+# i_rate_pos_glob_sent_polarity i_min_avg_negative_pol num_keywords num_videos
