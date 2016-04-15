85d95168dffb24f2a4154ab078e8d48075ecdef6
gbakie <gabriel.bakiewicz@gmail.com>
2016-04-15 00:01:23 -0400

    adding comments

--- Bootstrap.R
+++ Bootstrap.R
@@ -13,6 +13,7 @@ bootstrap <- function(formula, data) {
   
   models <- vector(mode="list", length=B)
   for (i in 1:B) {
+    # sample the same number of points with replacement
     boot_idx <- sample(n_rows, n_rows, replace = TRUE)
     boot_data <- data[boot_idx, ]
     
@@ -24,7 +25,7 @@ bootstrap <- function(formula, data) {
   return(models)
 }
 
-# stepwise selection
+# features from stepwise selection
 predictors <- c("data_channel", "cat_dow", "i_kw_max_avg_avg",
                 "self_reference_avg_sharess", "i_kw_avg_max_max",
                 "num_hrefs", "global_subjectivity", "LDA_00", 
@@ -55,8 +56,10 @@ news <- cat_encoding(news)
 url <- news$url
 news$url <- NULL
 
+# number of coefficients in the model
 N_COEF <- 31
 
+# get the coefficients values from each model
 coef <- matrix(nrow = B, ncol=N_COEF)
 models <- bootstrap(formula, news)
 for (i in 1:length(models)) {
@@ -65,15 +68,17 @@ for (i in 1:length(models)) {
   }
 }
 
+# train a model on the full dataset
 full_model <- lm(formula, data=news)
 full_coef <- vector(mode="list", length=N_COEF)
 predictor_names <- names(full_model$coefficients)[2:N_COEF]
 
+# get the coefficients of the full model
 for (i in 2:N_COEF) {
   full_coef[[i]] <- coef(full_model)[[i]]
 }
 
-
+# calculate coefficients confidence intervals
 coef_max <- vector(mode="list", length=N_COEF)
 coef_min <- vector(mode="list", length=N_COEF)
 for (i in 2:N_COEF) {
@@ -90,6 +95,7 @@ for (i in 2:N_COEF) {
               predictor_names[i], coef_min[[i]], coef_max[[i]]))  
 }
 
+# plot the coefficient and their confidence interval
 results = data.frame(name=predictor_names, coef=unlist(full_coef), max=unlist(coef_max), min=unlist(coef_min))
 
 ggplot(results, aes(x = name, y = coef)) +
@@ -98,6 +104,7 @@ ggplot(results, aes(x = name, y = coef)) +
   geom_errorbar(aes(ymax = max, ymin = min),width=0.1) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, size=10, face="bold"))
 
+# prediction for all the models
 pred <- matrix(nrow = nrow(news), ncol=B)
 for (i in 1:length(models)) {
   m <- models[[i]]
